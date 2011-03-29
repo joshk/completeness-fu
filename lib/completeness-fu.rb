26d9b4da212ceb6aa80288d30036019fd318c5e9
Josh Kalderimis <josh.kalderimis@gmail.com>
2011-03-29 10:34:13 +0200

    a little cleanup in the main lib file

--- lib/completeness-fu.rb
+++ lib/completeness-fu.rb
@@ -7,22 +7,30 @@ module CompletenessFu
     attr_accessor :default_i18n_namespace
     attr_accessor :default_gradings
   end
-end
 
-CompletenessFu.common_weightings  = { :low => 20, :medium => 40, :high => 60 }
-CompletenessFu.default_weighting  = 40
-CompletenessFu.default_i18n_namespace = [:completeness_scoring, :models]
-CompletenessFu.default_gradings = {
-  :poor => 0..24,
-  :low =>  25..49,
-  :medium => 50..79,
-  :high => 80..100
-}
+  self.common_weightings  = { :low => 20, :medium => 40, :high => 60 }
+
+  self.default_weighting  = 40
+
+  self.default_i18n_namespace = [:completeness_scoring, :models]
+
+  self.default_gradings = {
+    :poor => 0..24,
+    :low =>  25..49,
+    :medium => 50..79,
+    :high => 80..100
+  }
+
+  def self.setup_orm(orm_class)
+    orm_class.class_eval { include CompletenessFu::ActiveModelAdditions }
+  end
+
+end
 
 require 'completeness-fu/active_model_additions'
 require 'completeness-fu/scoring_builder'
 
 
-::ActiveRecord::Base.class_eval    { include CompletenessFu::ActiveModelAdditions } if defined?(::ActiveRecord::Base)
-::CassandraObject::Base.class_eval { include CompletenessFu::ActiveModelAdditions } if defined?(::CassandraObject::Base)
-::Mongoid::Document.class_eval     { include CompletenessFu::ActiveModelAdditions } if defined?(::Mongoid::Document)
+CompletenessFu.setup_orm(::ActiveRecord::Base)    if defined?(::ActiveRecord::Base)
+CompletenessFu.setup_orm(::CassandraObject::Base) if defined?(::CassandraObject::Base)
+CompletenessFu.setup_orm(::Mongoid::Document)     if defined?(::Mongoid::Document)
